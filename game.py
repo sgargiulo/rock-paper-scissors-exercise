b81c6729a434ee78dbf95ad77a461cb0a32caecd
Sean Gargiulo <spg363@stern.nyu.edu>
2019-06-05 19:25:13 -0400

    computer choice

--- game.py
+++ game.py
@@ -1,12 +1,15 @@
 # game.py
 
+import random
+
+
 print("Rock, Paper,Scissors, Shoot!") # you can also comment to the right
 
 # capture inputs
 
 user_choice = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes)")
-print("--------------")
 
+print("--------------")
 print("YOU CHOSE:", user_choice)
 
 
@@ -21,7 +24,14 @@ if user_choice not in ["rock", "paper", "scissors"]:
 
 print("GENERATING...")
 
+computer_choice = random.choice(["rock", "paper", "scissors"])
+
+print("--------------")
+print("COMPUTER CHOICE:", computer_choice)
+
 # determine the winner
+
+
 
 # display final outputs / outcomes
 
