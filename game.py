69b282ac57dd54bdadc06619342e6126e83ad580
Sean Gargiulo <spg363@stern.nyu.edu>
2019-06-05 19:46:25 -0400

    determine winning option

--- game.py
+++ game.py
@@ -2,7 +2,6 @@
 
 import random
 
-
 print("Rock, Paper,Scissors, Shoot!") # you can also comment to the right
 
 # capture inputs
@@ -32,6 +31,28 @@ print("COMPUTER CHOICE:", computer_choice)
 
 # determine the winner
 
+# rock beats scissors
+# paper beats rock
+# scissors beats paper
+# same selection is a tie
+
+if user_choice == computer_choice:
+    print("TIE")
+
+elif user_choice == "rock" and computer_choice == "paper":
+    print("PAPER")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("ROCK")
+
+elif user_choice == "paper" and computer_choice == "rock":
+    print("PAPER")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print("SCISSORS")
+
+elif user_choice == "scissors" and computer_choice == "rock":
+    print("ROCK")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("SCISSORS")
 
 
 # display final outputs / outcomes
