ebef971313d52da42b2c872fbe50f78ffcc81514
Sean Gargiulo <spg363@stern.nyu.edu>
2019-06-05 18:59:00 -0400

    capture inputs

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ print("Rock, Paper,Scissors, Shoot!") # you can also comment to the right
 user_choice = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes)")
 print("--------------")
 
-print("YOU CHOSE: user_choice")
+print("YOU CHOSE:", user_choice)
 
 
 
